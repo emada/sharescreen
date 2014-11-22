8e3438477f40e7d8136edb8900cdcfa38b597cc6
Emerson Lopes Machado <emerson@fantasticatecnologia.com.br>
2014-11-22 18:50:08 -0300

    Added the alinhamento presentation to be tested (to be removed)

--- screenshare_chrome_ext/contentscript.js
+++ screenshare_chrome_ext/contentscript.js
@@ -7,7 +7,8 @@ var special_keys = [112, 113, 114, 115, 117, 118, 119, 120, 121, 16, 18, 2, 20, 
 var unique_id = '';
 var connected = false;
 var listening_to_actions = false;
-
+var mouse_position = '';
+var last_mouse_position = '';
 
 $(document).ready(function() {
   WebSocketClient.init();
@@ -102,12 +103,12 @@ new function() {
   
   var onMessage = function(event) {
     var msg = JSON.parse(event.data);
-    // if (msg.id != unique_id) {
+    if (msg.id != unique_id) {
       $('#pointer_ema').show();
       act_on_me(msg);
-    // } else {
-      // $('#pointer_ema').hide();
-    // }
+    } else {
+      $('#pointer_ema').hide();
+    }
   };
   
   var onError = function(event) {
@@ -219,6 +220,7 @@ function act_on_me(msg) {
 
 function stop_listen_to_actions() {
   $(document).off('mousemove.share_screen_namespace');
+  window.clearTimeout(send_mouse_position_timer_ID);
   $(document).off('keydown.share_screen_namespace');
   $(document).off('keyup.share_screen_namespace');
   // $(document).off('mousewheel.share_screen_namespace DOMMouseScroll.share_screen_namespace');
@@ -227,18 +229,23 @@ function stop_listen_to_actions() {
 
 
 function start_listen_to_actions() {
-  // $(document).mousemove(function(event) {
   $(document).on('mousemove.share_screen_namespace', function(event) {
     var x = event.pageX - 4;
     var y = event.pageY - 3;
-    var position = "{left : " + x  + ", top  : " + y + "}"
-    var command = "$('#pointer_ema').offset(" + position + ")";
-    // console.log({type: 'call_function', value: command});
-    msg = create_message('call_function', command);
-    ws.send(msg);
+    mouse_position = "{left : " + x  + ", top  : " + y + "}"
   });
-  
-  // $(document).bind("keydown",function(event) {
+  function send_mouse_position() {
+    if (mouse_position != last_mouse_position) {
+      var command = "$('#pointer_ema').offset(" + mouse_position + ")";
+      // console.log({type: 'call_function', value: command});
+      msg = create_message('call_function', command);
+      ws.send(msg);
+      last_mouse_position = mouse_position;
+    }
+    send_mouse_position_timer_ID = window.setTimeout(send_mouse_position, 100);
+  }
+  send_mouse_position();
+
   $(document).on('keydown.share_screen_namespace', function(event) {
     if ($(':focus') && $(':focus').val()) {
       var entry = (event.keyCode ? event.keyCode : event.which);
@@ -250,7 +257,6 @@ function start_listen_to_actions() {
     }
   });
 
-  // $(document).bind("keyup",function(event) {
   $(document).on('keyup.share_screen_namespace', function(event) {
     if ($(':focus') && $(':focus').val()) {
       var entry = (event.keyCode ? event.keyCode : event.which);
@@ -271,16 +277,14 @@ function start_listen_to_actions() {
     }
   });
 
-  // $(document).on('mousewheel.share_screen_namespace DOMMouseScroll.share_screen_namespace', function(event) {
-  // $(document).bind("mousewheel DOMMouseScroll",function(event){
-  //   xy = getScrollXY();
-  //   var command = "window.scrollTo(" + xy[0] + "," + xy[1] + ");";
-  //   msg = create_message('call_function', command);
-  //   ws.send(msg);
-  // });
+  $(document).on('mousewheel.share_screen_namespace DOMMouseScroll.share_screen_namespace', function(event) {
+    xy = getScrollXY();
+    var command = "window.scrollTo(" + xy[0] + "," + xy[1] + ");";
+    msg = create_message('call_function', command);
+    ws.send(msg);
+  });
 
   $(document).on('click.share_screen_namespace', function(event) {
-  // $(document).click(function(event) {
     var clicked_span = $(event.target).getPath();
     msg = create_message('clicked_object', clicked_span);
     ws.send(msg);
@@ -355,238 +359,3 @@ function create_message(type_val, value_val) {
   msg = {type: type_val, value: value_val, id: unique_id};
   return JSON.stringify(msg);
 }
-
-
-
-
-
-
-
-
-
-// vvvvvvv FUNFANDO vvvvvvvvv
-
-// // background -> aqui
-// // - escutar por porta aberta pelo contentscript
-// // - executar ações de acordo com as mensagens que chegam do contentscript
-
-// if (!already_loaded) {
-
-//   // Insert command bar on top of the page
-//   var cursor_img = chrome.extension.getURL("resources/cursor_pointer.png");
-//   $('body').prepend('<img style="pointer-events: none; z-index: 9999" id="pointer_ema" width="15px" src="' + cursor_img + '" /> <div>   <label>User name:</label>   <input type="text" id="username" autocomplete="on" value=""/>   <button id="connectButton">Open</button>   <button id="disconnectButton">Close</button>   <label>Status:</label>   <span id="connectionStatus">CLOSED</span> </div> ');
-
-
-//   connection = new WebSocket("ws://localhost:8001")
-
-//   // if (!nickname) {  
-//     nickname = prompt("Choose a nickname");
-//   // } else {
-    
-
-//     connection.onopen = function() {
-//       console.log("Connection with websocket opened")
-//       connection.send(JSON.stringify({type: 'nickname', value: nickname}));
-
-//       setup_env();
-//     }
-//   // }
-
-
-
-
-//   // Act on received message
-//   connection.onmessage = function (event) {
-//     var msg = JSON.parse(event.data);
-//     console.log(msg);
-//     act_on_me(msg)
-//   }
-
-
-
-//   connection.onclose = function() {
-//     console.log("Connection closed")
-//   }
-
-
-//   connection.onerror = function() {
-//     console.error("Connection error")
-//   }
-
-
-
-
-
-//   function act_on_me(msg) {
-//     switch (msg.type) {
-//       case "call_function":
-//         eval(msg.value);
-//         break;
-      
-//       case "clicked_object":
-//         // Check if object has 'onClick' property 
-//         // var clicked_object = objects[msg.value];
-//         var clicked_object = $(msg.value);
-//         var function_called = clicked_object.attr('onClick');
-//         if (function_called) {
-//           eval(function_called);
-//           console.log('function_called: '+ function_called);
-//         } else {
-//           clicked_object.focus();
-//           console.log("changing focus to: " + clicked_object);
-//         }
-//         break;
-      
-//       case "keypressed":
-//         $(document.activeElement).val(msg.value);
-//         console.log(msg);
-//         break;
-
-//       case "message":
-//         var div = document.createElement("div");
-//         div.textContent = msg.value;
-//         document.body.appendChild(div);
-//         break;
-
-//       default:
-//         console.log("Don't know what to do with the type of message " + msg.type + "with value " + msg.value);
-//     }
-//   }
-
-//   setup_env();
-// }
-
-
-
-
-// function setup_env() {
-//   if (already_loaded) {
-//     return;
-//   }
-
-//   // map_objects();
-
-//   $(document).mousemove(function(event) {
-//     var x = event.pageX - 4;
-//     var y = event.pageY - 3;
-//     var position = "{left : " + x  + ", top  : " + y + "}"
-//     var command = "$('#pointer_ema').offset(" + position + ")";
-//     // console.log({type: 'call_function', value: command});
-//     connection.send(JSON.stringify({type: 'call_function', value: command}));
-//   });
-  
-//   $(document).bind("keydown",function(event) {
-//     if ($(':focus') && $(':focus').val()) {
-//       var entry = (event.keyCode ? event.keyCode : event.which);
-//       if (entry == 13) { //Enter keycode
-//         command = "$('form').submit();";
-//         connection.send(JSON.stringify({type: 'call_function',value: command}));
-//       }
-//     }
-//   });
-
-//   $(document).bind("keyup",function(event) {
-//     if ($(':focus') && $(':focus').val()) {
-//       var entry = (event.keyCode ? event.keyCode : event.which);
-//       if (entry == 13) { //Enter keycode
-//         if ($('#uri_to_go').is(":focus")) {
-//           goto_uri($('#uri_to_go').val());
-//         } else if ($(':focus').is("input")) {
-//           command = "$('form').submit();";
-//           connection.send(JSON.stringify({type: 'call_function',value: command}));
-//         }
-//       }
-//       if (!is_special_key(entry)) {
-//         entry = $(':focus').val();
-//         connection.send(JSON.stringify({type: 'keypressed',value: entry}));
-//       }
-//     }
-//   });
-
-//   // $(document).bind("mousewheel DOMMouseScroll",function(event){
-//   //   xy = getScrollXY();
-//   //   var command = "window.scrollTo(" + xy[0] + "," + xy[1] + ");";
-//   //   connection.send(JSON.stringify({type: 'call_function', value: command}));
-//   // });
-
-//   $(document).click(function(event) {
-//     // var clicked_span = $(event.target);
-//     // console.log(clicked_span);
-//     // $.each(objects, function(i, val) {
-//     //   if (objects[i].data() === clicked_span.data()) {
-//     //     connection.send(JSON.stringify({type: 'clicked_object', value: i}));
-//     //     return false;
-//     //   }
-//     // });
-//     var clicked_span = $(event.target).getPath();
-//     connection.send(JSON.stringify({type: 'clicked_object', value: clicked_span}));
-//   });
-
-//   already_loaded = true;
-// }
-
-
-
-// ///////////////////////////////////////////////////////
-// // 
-// // Auxiliary functions
-// // 
-// function load_jquery() {
-//   var newscript = document.createElement('script');
-//   newscript.type = 'text/javascript';
-//   newscript.async = true;
-//   newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
-//   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(newscript);
-// }
-
-// function getScrollXY() {
-//   var scrOfX = 0, scrOfY = 0;
-//   if( typeof( window.pageYOffset ) == 'number' ) {
-//     //Netscape compliant
-//     scrOfY = window.pageYOffset;
-//     scrOfX = window.pageXOffset;
-//   } else if( document.body && ( document.body.scrollLeft || document.body.scrollTop ) ) {
-//     //DOM compliant
-//     scrOfY = document.body.scrollTop;
-//     scrOfX = document.body.scrollLeft;
-//   } else if( document.documentElement && ( document.documentElement.scrollLeft || document.documentElement.scrollTop ) ) {
-//     //IE6 standards compliant mode
-//     scrOfY = document.documentElement.scrollTop;
-//     scrOfX = document.documentElement.scrollLeft;
-//   }
-//   return [ scrOfX, scrOfY ];
-// }
-
-// function is_special_key(key) {
-//   return $.inArray(key, special_keys) > -1;
-// }
-
-// // function map_objects() {
-// //   $('*').each(function(i) {
-// //     objects[i] = $(this);
-// //   });
-// // }
-
-
-// jQuery.fn.getPath = function () {
-//     if (this.length != 1) throw 'Requires one element.';
-
-//     var path, node = this;
-//     while (node.length) {
-//         var realNode = node[0], name = realNode.localName;
-//         if (!name) break;
-//         name = name.toLowerCase();
-
-//         var parent = node.parent();
-
-//         var siblings = parent.children(name);
-//         if (siblings.length > 1) { 
-//             name += ':eq(' + siblings.index(realNode) + ')';
-//         }
-
-//         path = name + (path ? '>' + path : '');
-//         node = parent;
-//     }
-
-//     return path;
-// };
